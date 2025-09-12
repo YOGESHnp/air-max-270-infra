067b58ab855c2c8e5b5bf3f709d73f5bcea95b1c
YOGESHnp <yogesnp.tc@gmail.com>
2025-09-12 23:42:58 +0530

    Modified eks, vpc variables under dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -25,6 +25,45 @@ module "github_oidc_role" {
   github_branch     = var.github_branch
 }
 
+# Elastic Kubernetes Cluster
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name            = var.cluster_name
+  node_group_name         = var.node_group_name
+  node_instance_type      = var.node_instance_type
+  desired_capacity        = var.desired_capacity
+  min_size                = var.min_size
+  max_size                = var.max_size
+  vpc_id                  = var.vpc_id
+  private_subnets         = var.private_subnets
+  public_subnets          = var.public_subnets
+  kubernetes_version      = var.kubernetes_version
+  endpoint_private_access = var.endpoint_private_access
+  endpoint_public_access  = var.endpoint_public_access
+
+  environment  = var.environment
+  project_name = var.project_name
+
+  tags = var.tags
+
+  # argocd/IRSA
+  argocd_namespace         = var.argocd_namespace
+  argocd_sa_name           = var.argocd_sa_name
+  argocd_attach_policy_arn = var.argocd_attach_policy_arn
+}
+
+# VPC module
+module "vpc" {
+  source               = "../../modules/vpc"
+  environment          = var.environment
+  project_name         = var.project_name
+  vpc_cidr             = "10.0.0.0/16"
+  azs                  = ["us-east-1a", "us-east-1b"]
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
 # Optional outputs
 output "ecr_repo_url" {
   value = module.landing_ecr.repository_url
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -11,11 +11,11 @@ variable "aws_profile" {
 }
 
 variable "environment" {
-  type    = string
+  type = string
 }
 
 variable "project_name" {
-  type    = string
+  type = string
 }
 
 # ECR module variables
@@ -32,7 +32,7 @@ variable "github_oidc_role_name" {
   type = string
 }
 variable "github_oidc_policy_arns" {
-  type = list(string)
+  type    = list(string)
   default = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"]
 }
 
@@ -56,4 +56,88 @@ variable "github_repo" {
 variable "github_branch" {
   type = string
 }
+
+# EKS module variables
+variable "cluster_name" {
+  type = string
+}
+
+variable "node_group_name" {
+  type = string
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.29"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+
+# EKS Autoscaling
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 3
+}
+
+# argocd/IRSA
+variable "argocd_namespace" {
+  type    = string
+  default = "argocd"
+}
+
+variable "argocd_sa_name" {
+  type    = string
+  default = "argocd-server"
+}
+
+variable "argocd_attach_policy_arn" {
+  type    = string
+  default = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+variable "endpoint_private_access" {
+  description = "Enable private API access to the EKS cluster"
+  type        = bool
+  default     = true
+}
+
+variable "endpoint_public_access" {
+  description = "Enable public API access to the EKS cluster"
+  type        = bool
+  default     = true
+}
+
+
 

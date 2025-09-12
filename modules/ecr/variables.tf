8b81d2a2baa996d4e6fc357e1f32c2b33cb7fbf1
YOGESHnp <yogesnp.tc@gmail.com>
2025-09-12 18:23:43 +0530

    Add modules ecr, github-oidc-role, oidc-provider

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,16 @@
+variable "repo_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
+
+variable "scan_on_push" {
+  description = "Enable image scan on push"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Tags for the repository"
+  type        = map(string)
+  default     = {}
+}

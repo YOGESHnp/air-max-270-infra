ee6ea55c3a4d98586893c6661208588a52821e6e
YOGESHnp <yogesnp.tc@gmail.com>
2025-09-22 11:19:00 +0530

    Clean branch: keep only observability_stack_infra folder

--- main.tf
+++
@@ -1,6 +0,0 @@
-# Who am I? (sanity check)
-data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
--- modules/eks/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "cluster_certificate_authority" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-
